a962a3585e74fe365a69c0898c8a92ce6e516aec
justin-chu <justinchu252@gmail.com>
2020-03-07 00:05:46 -0500

    Fix

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -44,17 +44,17 @@ class Contact extends Component {
 
         <h3 class="text-center mb-3">Email me!</h3>
 
-        <form name="contact" method="POST" data-netlify="true" netlify>
+        <form name="contact-form" method="post" data-netlify="true" netlify>
        
         <div class="col">
-      <input type="text" name="name" class="form-control" placeholder="Name">
+      <input type="text" name="name" id="name" class="form-control" placeholder="Name">
     </input></div>
     <div class="col">
-      <input type="email" name="email" class="form-control" placeholder="Email">
+      <input type="email" name="email" id="email" class="form-control" placeholder="Email">
     </input>
   </div>
-  <input type="text" name="subject" class="form-control" placeholder="Subject"></input>
-          <textarea name="message"></textarea>
+  <input type="text" name="subject" id="subject" class="form-control" placeholder="Subject"></input>
+          <textarea name="message" id="message"></textarea>
           <button type="submit">Send</button>
 
         </form>
--- src/index.js
+++ src/index.js
@@ -10,20 +10,6 @@ import icon from './assets/images/favicon.ico';
 
 ReactDOM.render(
   <div>
-    <form hidden name="contact" method="POST" data-netlify="true" netlify>
-       
-       <div class="col">
-     <input type="text" name="name" class="form-control" placeholder="Name">
-     </input></div>
-     <div class="col">
-     <input type="email" name="email" class="form-control" placeholder="Email">
-     </input>
-     </div>
-     <input type="text" name="subject" class="form-control" placeholder="Subject"></input>
-         <textarea name="message"></textarea>
-         <button type="submit">Send</button>
-     
-       </form>
     <Favicon url={icon} />
     <Main/>
   </div>, 
