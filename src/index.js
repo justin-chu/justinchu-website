5650227d7ab3c08c05a653b62fb279863ca4878a
justin-chu <justinchu252@gmail.com>
2020-03-06 23:36:16 -0500

    Temp again

--- src/index.js
+++ src/index.js
@@ -7,7 +7,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Favicon from 'react-favicon';
 import icon from './assets/images/favicon.ico';
 
-
+<form name="contact" method="POST" data-netlify="true" netlify>
+       
+       <div class="col">
+     <input type="text" name="name" class="form-control" placeholder="Name">
+     </input></div>
+     <div class="col">
+     <input type="email" name="email" class="form-control" placeholder="Email">
+     </input>
+     </div>
+     <input type="text" name="subject" class="form-control" placeholder="Subject"></input>
+         <textarea name="message"></textarea>
+         <button type="submit">Send</button>
+     
+       </form>
+       
 ReactDOM.render(
   <div>
     <Favicon url={icon} />
