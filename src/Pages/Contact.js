4c5b86cc1f99d33b82ce32ae5a3b03e539c74d4c
justin-chu <justinchu252@gmail.com>
2020-03-11 16:39:30 -0400

    Contact form changes

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -18,24 +18,22 @@ class Contact extends Component {
       message: '',
       sent: false,
       buttonText: 'Send Email',
-      error: ''
+      colour: 'primary'
     }
-  }
-  
-  resetForm(){
-    this.setState({name: '', email: '', subject: '', message: '', buttonText: 'Send Email'})
   }
 
   handleSubmit = e => {
+    this.setState({buttonText: 'Sending...', colour: 'secondary'})
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: encode({ "form-name": "contact", ...this.state })
     })
-      .then(() => alert("Success!"))
+      .then(() => this.setState({buttonText: 'Sent!', name: '', email: '', subject: '', message: '', colour: 'success'}))
       .catch(error => alert(error));
 
     e.preventDefault();
+    setTimeout(()=>{this.setState({buttonText: 'Send Email', colour: 'primary'})}, 3000);
   };
 
   render() {
@@ -43,21 +41,6 @@ class Contact extends Component {
       <div class="pl-5 pr-5 mt-5 mb-5">
 
         <h3 class="text-center mb-3">Email me!</h3>
-
-        {/* <form onSubmit={this.handleSubmit}>
-       
-        <div class="col">
-      <input type="text" name="name" id="name" class="form-control" placeholder="Name">
-    </input></div>
-    <div class="col">
-      <input type="email" name="email" id="email" class="form-control" placeholder="Email">
-    </input>
-  </div>
-  <input type="text" name="subject" id="subject" class="form-control" placeholder="Subject"></input>
-          <textarea name="message" id="message"></textarea>
-          <button type="submit">Send</button>
-
-        </form> */}
         <Form>
           <Form.Row>
             <div class="col-sm w-100">
@@ -74,7 +57,7 @@ class Contact extends Component {
           <Form.Control placeholder="Message" name="message" style={{marginBottom: 10}} value={this.state.message}
             onChange={(text) => {this.setState({message: text.target.value})}} as="textarea" rows="5" />
           <div class="text-center">
-            <Button onClick={this.handleSubmit.bind(this)} type="submit">{this.state.buttonText}</Button>
+            <Button variant={this.state.colour} onClick={this.handleSubmit.bind(this)} type="submit">{this.state.buttonText}</Button>
           </div>
         </Form>
 
